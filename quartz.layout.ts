d8f5174de43ccfabbc5c1a00464e51aad061debd
igjeoai <tomoki.e29@gmail.com>
2025-05-17 02:03:07 +0900

    update footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,13 +6,9 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.Footer({ links: {} }), // OK！
 }
+
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
